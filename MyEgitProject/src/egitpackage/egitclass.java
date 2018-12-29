888108ce99a669ed84c55043688aa067bdacbd0a
rajani <rajanisetti18@gmail.com>
2018-12-29 00:21:19 -0600

    commiting urgent changes

--- MyEgitProject/src/egitpackage/egitclass.java
+++ MyEgitProject/src/egitpackage/egitclass.java
@@ -3,10 +3,10 @@ package egitpackage;
 public class egitclass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 System.out.println("HI");
 System.out.println("Hello git");
 System.out.println("Hello egit");
+System.out.println("selenium1");
 
 	}
 
